d6daab17453206c21ea1f9b0d1b11aab3786a39e
Erik Visnievskij <erik903@mail.ru>
2022-08-01 08:36:05 +0300

    new

--- albums.js
+++ albums.js
@@ -1,24 +1,91 @@
-fetch('https://jsonplaceholder.typicode.com/albums?_limit=15')
-  .then(res => res.json())
-  .then(albums => {
-    let albumsWrapper = document.querySelector('#albums-wrapper');
-    albums.map(album => {
-      let albumItem = document.createElement('div');
-      albumItem.classList.add('album-item');
+let albumsWrapper = document.querySelector('#albums-wrapper');
+let albumsWrapperTitle = document.createElement('h2');
+document.body.prepend(albumsWrapperTitle);
 
-      fetch('https://jsonplaceholder.typicode.com/users/' + album.userId)
-        .then(res => res.json())
-        .then(user => {
+function init() {
+  let queryParams = document.location.search;
+  let urlParams = new URLSearchParams(queryParams);
+  let userId = urlParams.get('user_id');
 
-          fetch(`https://jsonplaceholder.typicode.com/albums/${album.id}/photos?_limit=1`)
-            .then(res => res.json())
-            .then(photos => {
-              albumItem.innerHTML = `<h3><a href="./album.html?album_id=${album.id}&album_title=${album.title}&user_id=${album.userId}&user_name=${user.name}">${album.title}</a></h3>
-                                     <div>Album created by: ${user.name}</div>
-                                     <img src="${photos[0].thumbnailUrl}">`;
-            })
-        })
+  if (userId) {
+    renderAlbumsByUserId(userId);
+  } else {
+    renderAllAlbums();
+  }
+}
 
-        albumsWrapper.prepend(albumItem);
+function renderAlbumsByUserId(id) {
+  fetch(`https://jsonplaceholder.typicode.com/users/${id}/albums?_embed=photos&_expand=user`)
+    .then(res => res.json())
+    .then(albums => {
+      albums.map(singleAlbum => {
+        // let albumItem = document.createElement('div');
+        // albumItem.classList.add('album-item');
+        
+        // albumsWrapperTitle.textContent = `Albums of ${album.user.name}:`;
+            
+        // let randomIndex = Math.floor(Math.random() * album.photos.length);
+
+        // albumItem.innerHTML = `<h3><a href="./album.html?album_id=${album.id}&album_title=${album.title}&user_id=${album.userId}&user_name=${album.user.name}">${album.title}</a> (${album.photos.length})</h3>
+        // <img src="${album.photos[randomIndex].thumbnailUrl}">`;
+
+        // albumsWrapper.prepend(albumItem);
+
+        let albumData = {
+          album: singleAlbum,
+          title: `Albums of ${singleAlbum.user.name}:`,
+          createdBy: '',
+        }
+
+        renderSingleAlbum(albumData);
+      })
     })
-  })
+}
+
+function renderAllAlbums() {
+  fetch('https://jsonplaceholder.typicode.com/albums?_expand=user&_embed=photos&_limit=15')
+    .then(res => res.json())
+    .then(albums => {
+      console.log(albums);
+      albums.map(singleAlbum => {
+
+        // let albumData = {
+        //   album: singleAlbum,
+        //   title: 'All albums:',
+        //   createdBy: `<div>Album created by: <a href="./user.html?user_id=${singleAlbum.user.id}">${singleAlbum.user.name}</a></div>`,
+        // }
+
+        // renderSingleAlbum(albumData);
+
+        renderSingleAlbum({
+          album: singleAlbum,
+          title: 'All albums:',
+          createdBy: `<div>Album created by: <a href="./user.html?user_id=${singleAlbum.user.id}">${singleAlbum.user.name}</a></div>`,
+        });
+      })
+    })
+}
+
+function renderSingleAlbum(data) {
+  // let album = data.album;
+  // let title = data.title;
+  // let createdBy = data.createdBy;
+  
+  let {album, title, createdBy} = data;
+
+  let albumItem = document.createElement('div');
+  albumItem.classList.add('album-item');
+  
+  albumsWrapperTitle.textContent = title;
+
+  let randomIndex = Math.floor(Math.random() * album.photos.length);
+
+  albumItem.innerHTML = `<h3><a href="./album.html?album_id=${album.id}&album_title=${album.title}&user_id=${album.userId}&user_name=${album.user.name}">${album.title}</a> (${album.photos.length})</h3>
+                        ${createdBy}
+                        <img src="${album.photos[randomIndex].thumbnailUrl}">`;
+
+  albumsWrapper.prepend(albumItem);
+}
+
+init();
+
---
+++ posts.js
@@ -0,0 +1,49 @@
+let postsWrapper = document.querySelector('#posts-wrapper');
+let postsListTitle = document.createElement('h2');
+let postsList = document.createElement('ul');
+
+postsWrapper.append(postsListTitle, postsList);
+
+function init () { 
+  let queryParams = document.location.search;
+  let urlParams = new URLSearchParams(queryParams);
+  let userId = urlParams.get('user_id');
+
+  if (userId) {
+    renderPostsByUserId(userId);
+  } else {
+    renderAllPosts();
+  }
+}
+
+function renderPostsByUserId(id) {
+
+  fetch(`https://jsonplaceholder.typicode.com/users/${id}?_embed=posts`)
+    .then(res => res.json())
+    .then(user => {
+      postsListTitle.textContent = `Posts of ${user.name}:`;
+      
+      user.posts.map(post => {
+        let postItem = document.createElement('li');
+        postItem.innerHTML = `<a href="./post.html?post_id=${post.id}">${post.title}</a>`;
+
+        postsList.prepend(postItem);
+      })
+    })
+}
+
+function renderAllPosts() {
+  fetch('https://jsonplaceholder.typicode.com/posts?_expand=user')
+    .then(res => res.json())
+    .then(posts => {
+      postsListTitle.textContent = 'All Posts:';
+      posts.map(post => {
+        let postItem = document.createElement('li');
+        postItem.innerHTML = `<a href="./post.html?post_id=${post.id}">${post.title} (${post.user.name})</a>`;
+
+        postsList.prepend(postItem);
+      })
+    })
+}
+
+init();
